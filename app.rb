ad427e1627b623693004629a7971606a04f73147
Andrew Davey <ajdavey8@gmail.com>
2017-11-07 16:28:00 +0000

    Adds class in app file and updates requirements, adds config file

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/battle' do
-  "Hello Battle!"
+class Battle < Sinatra::Base
+  get '/battle' do
+    "Hello Battle!"
+  end
+
+  run! if app_file == $0
 end
