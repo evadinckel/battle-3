2e2d3f0674586a99a4253db20f1897ff7e243d44
Eva.Dinckel <evadinckel@gmail.com>
2017-11-07 18:06:21 +0000

    created a form including 2 players, and a post method to get 2 names

--- app.rb
+++ app.rb
@@ -6,4 +6,15 @@ class Battle < Sinatra::Base
   end
 
   run! if app_file == $0
+
+
+get '/' do
+  erb(:index)
+end
+
+post '/names' do
+  @player_1 = params[:player_1]
+  @player_2 = params[:player_2]
+  erb(:play)
+end
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -7,3 +7,11 @@ RSpec.feature "battle page test", :type => :feature do
 expect(page).to have_content('Testing infrastructure working!')
 end
 end
+
+RSpec.feature "battle page test", :type => :feature do
+  scenario "Viewing the page" do
+    visit "/"
+    fill_in('player_1', with: 'eva')
+    fill_in('player_2', with: 'andrew')
+  end
+end
