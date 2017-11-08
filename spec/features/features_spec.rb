052df8250db7e743e5def6b1e3c4fc6b9549c3f6
Eva.Dinckel <evadinckel@gmail.com>
2017-11-08 16:53:03 +0000

    adding a attack player button, not done with confo button yet

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,11 +1,5 @@
 require './app.rb'
 
-# let(:named_players)do
-#   visit "/"
-#   fill_in('player_1', with: 'eva')
-#   fill_in('player_2', with: 'andrew')
-#   click_button "Submit"
-# end
 
 RSpec.feature "battle page test", :type => :feature do
   scenario "Viewing the page" do
@@ -15,32 +9,32 @@ RSpec.feature "battle page test", :type => :feature do
 end
 
 RSpec.feature "enter player names" do
-  let(:named_players)do
-    visit "/"
-    fill_in('player_1', with: 'eva')
-    fill_in('player_2', with: 'andrew')
-    click_button "Submit"
-  end
   scenario "Getting player two's name" do
-    named_players
+    sign_in_and_play
     expect(page).to have_content('andrew')
   end
   scenario "Getting player one's name" do
-    named_players
+    sign_in_and_play
     expect(page).to have_content('eva')
   end
 end
 
 RSpec.feature "viewing hit points" do
-  let(:named_players)do
-    visit "/"
-    fill_in('player_1', with: 'eva')
-    fill_in('player_2', with: 'andrew')
-    click_button "Submit"
-  end
   scenario "allowing player 1 to see player 2's hit points" do
-    named_players
+    sign_in_and_play
     expect(page).to have_content('hit points: 100')
   end
+end
 
+RSpec.feature "attacking a player" do
+  scenario "checking the attack button exists" do
+    sign_in_and_play
+    expect(page).to have_selector(:link_or_button, 'Attack!')
+  end
+  scenario "clicking button to attack and get confirmation" do
+    sign_in_and_play
+    click_button('Attack!')
+    window = switch_to_window { title == 'Attacking player 2' }
+    expect(window).to have_selector(:link_or_button, 'Ok')
+  end
 end
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,7 @@
+
+def sign_in_and_play
+  visit "/"
+  fill_in('player_1', with: 'eva')
+  fill_in('player_2', with: 'andrew')
+  click_button "Submit"
+end
