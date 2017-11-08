eb498dac768137efa593dadc35dc468924694879
Eva.Dinckel <evadinckel@gmail.com>
2017-11-08 14:49:26 +0000

    adding sessions to our app file to keep players names and testing play

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,14 +4,23 @@ require './app.rb'
 RSpec.feature "battle page test", :type => :feature do
   scenario "Viewing the page" do
     visit "/battle"
-expect(page).to have_content('Testing infrastructure working!')
-end
+    expect(page).to have_content('Testing infrastructure working!')
+  end
 end
 
-RSpec.feature "battle page test", :type => :feature do
-  scenario "Viewing the page" do
+RSpec.feature "play page test", :type => :feature do
+  scenario "Getting player two's name" do
     visit "/"
     fill_in('player_1', with: 'eva')
     fill_in('player_2', with: 'andrew')
+    click_button "Submit"
+    expect(page).to have_content('andrew')
+  end
+  scenario "Getting player one's name" do
+    visit "/"
+    fill_in('player_1', with: 'eva')
+    fill_in('player_2', with: 'andrew')
+    click_button "Submit"
+    expect(page).to have_content('eva')
   end
 end
